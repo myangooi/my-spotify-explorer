ce6f29c5e2325b01c0ea10111321375e4816f4dd
Ming Yang <ooimingyang0920@gmail.com>
2024-06-14 11:07:46 +0800

    updateed config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const isProd = process.env.NODE_ENV === "production";
+
+const nextConfig = {
+  assetPrefix: isProd ? "/my-spotify-explorer/" : "",
+  basePath: isProd ? "/my-spotify-explorer" : "",
+};
 
 export default nextConfig;
