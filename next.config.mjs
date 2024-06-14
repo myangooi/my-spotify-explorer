b44434536949ef58b0ab2d6171c94c8ab46af14d
Ming Yang <ooimingyang0920@gmail.com>
2024-06-14 10:57:20 +0800

    removed config

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
-  basePath: "/my-spotify-explorer",
-  assetPrefix: "/my-spotify-explorer/",
-};
+const nextConfig = {};
 
 export default nextConfig;
