8fc9d6fc0fe5cfd27e2db87542144ad19a966f70
Ming Yang <mingyang@netizenexperience.com>
2024-06-14 10:29:23 +0800

    updated config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/my-spotify-explorer",
+  assetPrefix: "/my-spotify-explorer/",
+};
 
 export default nextConfig;
