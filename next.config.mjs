5d7db8da2578aee46d36f4d06b5ad56c260090e7
Ming Yang <ooimingyang0920@gmail.com>
2024-06-16 15:09:54 +0800

    updated config and package

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,13 @@ const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   assetPrefix: isProd ? "/my-spotify-explorer" : "",
   basePath: isProd ? "/my-spotify-explorer" : "",
+  env: {
+    CLIENT_ID: process.env.CLIENT_ID,
+    REDIRECT_URL: process.env.REDIRECT_URL,
+    AUTHORIZATION_URL: process.env.AUTHORIZATION_URL,
+    TOKEN_URL: process.env.TOKEN_URL,
+    SCOPE: process.env.SCOPE,
+  },
 };
 
 export default nextConfig;
