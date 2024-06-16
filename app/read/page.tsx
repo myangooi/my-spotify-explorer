14b29fe6ed00e6898f184599fb70d44451903107
Ming Yang <ooimingyang0920@gmail.com>
2024-06-16 17:00:30 +0800

    updated read page

--- app/read/page.tsx
+++ app/read/page.tsx
@@ -5,35 +5,36 @@ import CONST from "../constants";
 
 // Get access token and redirect to profile page
 export default function Read() {
-  const urlParams = new URLSearchParams(window.location.search);
-  let code = urlParams.get("code") ?? "";
-
-  async function getAccessToken() {
-    let codeVerifier = window.localStorage.getItem("code_verifier");
-
-    const payload = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-      body: new URLSearchParams({
-        grant_type: "authorization_code",
-        code: code,
-        redirect_uri: CONST.REDIRECT_URL ?? "",
-        client_id: process.env.CLIENT_ID ?? "",
-        code_verifier: codeVerifier ?? "",
-      }),
-    };
-
-    const body = await fetch(CONST.TOKEN_URL, payload);
-    const response = await body.json();
-    window.localStorage.setItem("access_token", response.access_token);
-  }
-
   useEffect(() => {
-    getAccessToken();
-    window.location.href = "/profile";
-  });
+    const urlParams = new URLSearchParams(window.location.search);
+    let code = urlParams.get("code") ?? "";
+
+    async function getAccessToken() {
+      let codeVerifier = window.localStorage.getItem("code_verifier");
+
+      const payload = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: new URLSearchParams({
+          grant_type: "authorization_code",
+          code: code,
+          redirect_uri: CONST.REDIRECT_URL ?? "",
+          client_id: process.env.CLIENT_ID ?? "",
+          code_verifier: codeVerifier ?? "",
+        }),
+      };
+
+      const body = await fetch(CONST.TOKEN_URL, payload);
+      const response = await body.json();
+      window.localStorage.setItem("access_token", response.access_token);
+    }
+
+    getAccessToken().then(() => {
+      window.location.href = "/profile";
+    });
+  }, []);
 
   return <></>;
 }
