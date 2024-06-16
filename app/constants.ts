cd22e9fd4f2b43869285da01097ab46685138299
Ming Yang <ooimingyang0920@gmail.com>
2024-06-17 00:49:55 +0800

    added base url const

--- app/constants.ts
+++ app/constants.ts
@@ -1,4 +1,5 @@
 export const CONST = {
+  BASE_URL: process.env.NODE_ENV === "production" ? "/my-spotify-explorer" : "",
   AUTHORIZATION_URL: "https://accounts.spotify.com/authorize",
   TOKEN_URL: "https://accounts.spotify.com/api/token",
   SCOPE: "user-top-read",
