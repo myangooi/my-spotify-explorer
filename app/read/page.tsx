19baa577e6e64bb2f68c5d5d5de556ce4127e1e1
Ming Yang <ooimingyang0920@gmail.com>
2024-06-16 16:39:47 +0800

    added get access token method

--- app/read/page.tsx
+++ app/read/page.tsx
@@ -1,9 +1,37 @@
-import ProfileLayout from "../profile/layout";
+"use client";
+
+import { useEffect } from "react";
 
 export default function Read() {
-  return (
-    <div>
-      <ProfileLayout />
-    </div>
-  );
+  const urlParams = new URLSearchParams(window.location.search);
+  let code = urlParams.get("code") ?? "";
+
+  async function getAccessToken() {
+    let codeVerifier = window.localStorage.getItem("code_verifier");
+
+    const payload = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: new URLSearchParams({
+        grant_type: "authorization_code",
+        code: code,
+        redirect_uri: process.env.REDIRECT_URL ?? "",
+        client_id: process.env.CLIENT_ID ?? "",
+        code_verifier: codeVerifier ?? "",
+      }),
+    };
+
+    const body = await fetch("https://accounts.spotify.com/api/token", payload);
+    const response = await body.json();
+    window.localStorage.setItem("access_token", response.access_token);
+  }
+
+  useEffect(() => {
+    getAccessToken();
+    window.location.href = "/profile";
+  });
+
+  return <></>;
 }
