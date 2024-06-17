1700b465377bdc1400e2f53da1f8e1d349fdd3f7
Ming Yang <ooimingyang0920@gmail.com>
2024-06-17 22:40:05 +0800

    added page guard for profile

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,14 +1,16 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import NavBar from "../components/NavBar/NavBar";
 import { TopArtistsResponse, TopTracksResponse } from "../interfaces/api-top";
 import TopTracks from "../components/TopItems/Tracks/TopTracks";
 import GetTopItems, { TimeRange, Type } from "../components/Button/GetTopItems";
 import TopTypeSelect from "../components/Select/TopTypeSelect";
 import TopArtists from "../components/TopItems/Artists/TopArtists";
+import CONST from "../constants";
 
 export default function Profile() {
+  const [isLogin, setIsLogin] = useState(false);
   const [topItems, setItems] = useState<
     TopTracksResponse | TopArtistsResponse | null
   >(null);
@@ -33,47 +35,59 @@ export default function Profile() {
     setTimeRange(timeRange);
   }
 
+  useEffect(() => {
+    if (window.localStorage.getItem("access_token") === null) {
+      window.location.href = `${CONST.BASE_URL}/`;
+    } else {
+      setIsLogin(true);
+    }
+  }, [isLogin]);
+
   return (
     <>
-      <NavBar />
-      <div className="relative top-16">
-        <div className="flex flex-row gap-4 flex-wrap justify-center py-3">
-          <TopTypeSelect handleMode={handleMode} />
-          <div className="flex flex-row justify-center gap-2">
-            <GetTopItems
-              timeRange="short_term"
-              type={mode}
-              handleFetch={handleFetch}
-              handleTimeRange={handleTimeRange}
-            />
-            <GetTopItems
-              timeRange="medium_term"
-              type={mode}
-              handleFetch={handleFetch}
-              handleTimeRange={handleTimeRange}
-            />
-            <GetTopItems
-              timeRange="long_term"
-              type={mode}
-              handleFetch={handleFetch}
-              handleTimeRange={handleTimeRange}
-            />
+      {isLogin ? (
+        <>
+          <NavBar />
+          <div className="relative top-16">
+            <div className="flex flex-row gap-4 flex-wrap justify-center py-3">
+              <TopTypeSelect handleMode={handleMode} />
+              <div className="flex flex-row justify-center gap-2">
+                <GetTopItems
+                  timeRange="short_term"
+                  type={mode}
+                  handleFetch={handleFetch}
+                  handleTimeRange={handleTimeRange}
+                />
+                <GetTopItems
+                  timeRange="medium_term"
+                  type={mode}
+                  handleFetch={handleFetch}
+                  handleTimeRange={handleTimeRange}
+                />
+                <GetTopItems
+                  timeRange="long_term"
+                  type={mode}
+                  handleFetch={handleFetch}
+                  handleTimeRange={handleTimeRange}
+                />
+              </div>
+            </div>
+            {mode === "tracks" ? (
+              <TopTracks
+                topTracks={topItems as TopTracksResponse}
+                timeRange={timeRange}
+              />
+            ) : mode === "artists" ? (
+              <TopArtists
+                topArtists={topItems as TopArtistsResponse}
+                timeRange={timeRange}
+              />
+            ) : (
+              <></>
+            )}
           </div>
-        </div>
-        {mode === "tracks" ? (
-          <TopTracks
-            topTracks={topItems as TopTracksResponse}
-            timeRange={timeRange}
-          />
-        ) : mode === "artists" ? (
-          <TopArtists
-            topArtists={topItems as TopArtistsResponse}
-            timeRange={timeRange}
-          />
-        ) : (
-          <></>
-        )}
-      </div>
+        </>
+      ) : null}
     </>
   );
 }
