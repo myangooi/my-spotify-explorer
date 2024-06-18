ee449d11bedf3f7794b0975ea6fe1e58ce710717
Ming Yang <ooimingyang0920@gmail.com>
2024-06-18 12:32:38 +0800

    update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,15 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import CONST from "./shared/constants";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "My Spotify Explorer",
-  description: "Spotify Stuffs at a Glance",
+  description: "Spotify Stuff at a Glance",
   icons: "/favicon.ico",
+  metadataBase: new URL(CONST.BASE_URL),
 };
 
 export default function RootLayout({
