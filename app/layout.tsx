8693350a31197b21b46bf9fa4df390dbfc25602b
Ming Yang <ooimingyang0920@gmail.com>
2024-06-21 14:52:16 +0800

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "My Spotify Explorer",
+  title: "Spotify Explorer",
   description: "Spotify Stuff at a Glance",
   icons: "./favicon.ico",
   metadataBase: new URL("https://myangooi.github.io/"),
